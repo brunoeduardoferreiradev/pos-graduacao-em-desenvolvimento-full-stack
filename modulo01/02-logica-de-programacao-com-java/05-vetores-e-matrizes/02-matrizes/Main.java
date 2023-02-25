11f882ed26f4b555f92b7b7a1f88a9f8cd56208d
Bruno Eduardo de Moura Ferreira <125761885+brunoeduardoferreiradev@users.noreply.github.com>
2023-02-25 20:49:16 -0300

    Aula : Matrizes

---
+++ modulo01/02-logica-de-programacao-com-java/05-vetores-e-matrizes/02-matrizes/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+  public static void main(String[] args) {
+    int[][] dados = new int[3][3];
+
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        System.out.println(dados[i][j] = j);
+      }
+    }
+  }
+}
