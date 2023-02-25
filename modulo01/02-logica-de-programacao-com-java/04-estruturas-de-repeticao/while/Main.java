918d2ffea1f9e01019de0ac56fdb428ad16f1df5
Bruno Eduardo de Moura Ferreira <125761885+brunoeduardoferreiradev@users.noreply.github.com>
2023-02-25 11:26:23 -0300

    Aula : Estruturas de Repetição - While

---
+++ modulo01/02-logica-de-programacao-com-java/04-estruturas-de-repeticao/while/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    int opcao = 0;
+
+    while (opcao != 99) {
+      System.out.println("Digite um valor qualquer ou 99 para sair");
+      Scanner entrada = new Scanner(System.in);
+      opcao = entrada.nextInt();
+    }
+  }
+
+}
