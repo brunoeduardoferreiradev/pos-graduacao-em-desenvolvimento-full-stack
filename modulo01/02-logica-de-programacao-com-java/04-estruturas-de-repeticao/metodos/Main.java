e59178515bf42d31fa8db34171850d31c61dc5ef
Bruno Eduardo de Moura Ferreira <125761885+brunoeduardoferreiradev@users.noreply.github.com>
2023-02-25 13:30:19 -0300

    Aula : Métodos

---
+++ modulo01/02-logica-de-programacao-com-java/04-estruturas-de-repeticao/metodos/Main.java
@@ -0,0 +1,12 @@
+import java.io.Console;
+
+public class Main {
+  public static int somar(int a, int b) {
+    return (a + b);
+  }
+
+  public static void main(String[] args) {
+    int total = Main.somar(10, 20);
+    System.out.println("Total é " + total);
+  }
+}
