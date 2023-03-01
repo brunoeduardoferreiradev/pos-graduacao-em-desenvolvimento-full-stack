3213f08cdffb788809c14ff1ae6e9977ec5ee321
Bruno Eduardo de Moura Ferreira <125761885+brunoeduardoferreiradev@users.noreply.github.com>
2023-03-01 01:24:41 -0300

    Aula : Operadores Aritmeticos

---
+++ modulo-01/src/LogicaDeProgramacaoEmJava/IntroducaoALinguagemJava/OperadoresAritmeticos/Main.java
@@ -0,0 +1,37 @@
+package LogicaDeProgramacaoEmJava.IntroducaoALinguagemJava.OperadoresAritmeticos;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double numA, numB, total;
+
+        numA = 8;
+        numB = 3;
+
+        // Soma
+        total = numA + numB;
+        System.out.println("Soma de A + B = " + total);
+
+        // Subtração
+        total = numA - numB;
+        System.out.println("Subtração de A - B = " + total);
+
+        // Multiplicação
+        total = numA * numB;
+        System.out.println("Multiplicação de A * B = " + total);
+
+        // Divisão
+        total = numA / numB;
+        System.out.println("Divisão de A / B = " + total);
+
+        // Resto da Divisão
+        total = numA % numB;
+        System.out.println("Resto de A % B = " + total);
+
+        // Calculo de Potencia ( 4³ )
+        total = 4 * 4 * 4;
+        System.out.println("Potencia de 4³ = " + total);
+    }
+
+}
+
