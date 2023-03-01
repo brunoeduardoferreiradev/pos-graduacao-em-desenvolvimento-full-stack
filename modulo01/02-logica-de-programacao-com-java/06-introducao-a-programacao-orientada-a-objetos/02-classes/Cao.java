2b678fcd1d9efddda68f61cbc4e4fd2511b05e6b
Bruno Eduardo de Moura Ferreira <125761885+brunoeduardoferreiradev@users.noreply.github.com>
2023-02-28 23:57:06 -0300

    Aula : Classes

---
+++ modulo01/02-logica-de-programacao-com-java/06-introducao-a-programacao-orientada-a-objetos/02-classes/Cao.java
@@ -0,0 +1,11 @@
+public class Cao {
+  String nome;
+  String cor;
+  int idade;
+  double peso;
+
+  public void Anda() {
+    System.out.println("Estouy andando...");
+  }
+
+}
---
+++ modulo01/02-logica-de-programacao-com-java/06-introducao-a-programacao-orientada-a-objetos/02-classes/Main.java
@@ -0,0 +1,10 @@
+import Cao;
+
+public class Main {
+  public static void main(String[] args) {
+    Cao cachorro = new Cao();
+    cachorro.nome = "Bobby";
+    cachorro.Anda();
+
+  }
+}
