6b82eb01dca49a7e1b8783363ec3f7dec63bae6f
Bruno Eduardo de Moura Ferreira <125761885+brunoeduardoferreiradev@users.noreply.github.com>
2023-03-01 01:21:10 -0300

    Aula : Variaveis e Tipos de Dados

---
+++ modulo-01/src/LogicaDeProgramacaoEmJava/IntroducaoALinguagemJava/VariaveisETiposDeDados/Main.java
@@ -0,0 +1,9 @@
+package LogicaDeProgramacaoEmJava.IntroducaoALinguagemJava.VariaveisETiposDeDados;
+
+public class Main {
+    public static void main(String[] args) {
+        int numero;
+        float saldo;
+        String nome;
+    }
+}
