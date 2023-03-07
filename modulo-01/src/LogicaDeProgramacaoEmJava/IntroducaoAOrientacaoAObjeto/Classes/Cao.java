d5d8b6159eee8405d236782dbefff6dc9884c457
Bruno Eduardo de Moura Ferreira <125761885+brunoeduardoferreiradev@users.noreply.github.com>
2023-03-06 23:40:00 -0300

    Aula de Classes
    Aula de Classes

---
+++ modulo-01/src/LogicaDeProgramacaoEmJava/IntroducaoAOrientacaoAObjeto/Classes/Cao.java
@@ -0,0 +1,12 @@
+package LogicaDeProgramacaoEmJava.IntroducaoAOrientacaoAObjeto.Classes;
+
+public class Cao {
+    String nome;
+    String cor;
+    int idade;
+    double peso;
+
+    public void Anda() {
+      System.out.println("Estou Andando");
+    }
+}
---
+++ modulo-01/src/LogicaDeProgramacaoEmJava/IntroducaoAOrientacaoAObjeto/Classes/Main.java
@@ -0,0 +1,8 @@
+package LogicaDeProgramacaoEmJava.IntroducaoAOrientacaoAObjeto.Classes;
+
+public class Main {
+    public static void main(String[] args) {
+        Cao cao = new Cao();
+        cao.Anda();
+    }
+}
