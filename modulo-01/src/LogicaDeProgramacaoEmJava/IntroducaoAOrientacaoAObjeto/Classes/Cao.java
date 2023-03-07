8f295e2e37282bbdf4207cb206fe06de17d86029
Bruno Eduardo de Moura Ferreira <125761885+brunoeduardoferreiradev@users.noreply.github.com>
2023-03-06 23:45:49 -0300

    Aula : Constutores padrão e sobrecarregados

--- modulo-01/src/LogicaDeProgramacaoEmJava/IntroducaoAOrientacaoAObjeto/Classes/Cao.java
+++ modulo-01/src/LogicaDeProgramacaoEmJava/IntroducaoAOrientacaoAObjeto/Classes/Cao.java
@@ -6,7 +6,22 @@ public class Cao {
     int idade;
     double peso;
 
+    // Metodo construtor sempre é public
+    public Cao(){
+        cor = "Caramelo";
+    }
+
+    public Cao(String nome, int idade ){
+        this.nome =  nome;
+        this.idade = idade;
+    }
+
+
     public void Anda() {
-      System.out.println("Estou Andando");
+      System.out.println("Estou Andando ");
+    }
+
+    public void DadosCao(){
+        System.out.print( nome + " " + idade);
     }
 }
--- modulo-01/src/LogicaDeProgramacaoEmJava/IntroducaoAOrientacaoAObjeto/Classes/Main.java
+++ modulo-01/src/LogicaDeProgramacaoEmJava/IntroducaoAOrientacaoAObjeto/Classes/Main.java
@@ -4,5 +4,11 @@ public class Main {
     public static void main(String[] args) {
         Cao cao = new Cao();
         cao.Anda();
+
+        Cao dog = new Cao("Bob", 12);
+        dog.DadosCao();
+
     }
+
+
 }
