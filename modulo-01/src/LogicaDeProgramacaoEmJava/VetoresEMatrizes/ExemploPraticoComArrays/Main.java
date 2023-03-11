210d195994af85ee88d16c79ed4242f52fe4096b
Bruno Eduardo de Moura Ferreira <125761885+brunoeduardoferreiradev@users.noreply.github.com>
2023-03-10 22:36:44 -0300

    Aula Exemplo Pratico com Arrays

---
+++ modulo-01/src/LogicaDeProgramacaoEmJava/VetoresEMatrizes/ExemploPraticoComArrays/Main.java
@@ -0,0 +1,28 @@
+package LogicaDeProgramacaoEmJava.VetoresEMatrizes.ExemploPraticoComArrays;
+
+import java.util.ArrayList;
+
+public class Main {
+  public static void main(String[] args) {
+    ArrayList<String> bandas = new ArrayList<>();
+
+    bandas.add("Kiss");
+    bandas.add("Beatles");
+    bandas.add("Iron Maiden");
+    bandas.add("AC/DC");
+
+    System.out.println(bandas);
+
+    bandas.add(bandas.indexOf("Kiss"), "System of a Down");
+
+    System.out.println(bandas);
+
+    bandas.remove("Kiss");
+
+    System.out.println(bandas);
+
+    bandas.clear();
+
+    System.out.println(bandas);
+  }
+}
