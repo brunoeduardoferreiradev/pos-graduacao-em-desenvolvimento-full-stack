9bdba053957f9ecf9852d7ca068ef069585899ac
Bruno Eduardo de Moura Ferreira <125761885+brunoeduardoferreiradev@users.noreply.github.com>
2023-03-10 22:21:04 -0300

    Aula de For

---
+++ modulo-01/src/LogicaDeProgramacaoEmJava/EstruturasDeRepeticao/EstruturaFor/Main.java
@@ -0,0 +1,11 @@
+package LogicaDeProgramacaoEmJava.EstruturasDeRepeticao.EstruturaFor;
+import java.io.Console;
+
+class Main {
+
+  public static void main(String[] args) {
+    for (int count = 5; count < 20; count++) {
+      System.out.println("Números de 5 a 20 : " + count);
+    }
+  }
+}
