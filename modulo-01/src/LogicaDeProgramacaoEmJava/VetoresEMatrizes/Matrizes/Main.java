c489ab0406053af3e95f481ab46339f162bb9d69
Bruno Eduardo de Moura Ferreira <125761885+brunoeduardoferreiradev@users.noreply.github.com>
2023-03-10 22:35:47 -0300

    Aula de Matrizes

---
+++ modulo-01/src/LogicaDeProgramacaoEmJava/VetoresEMatrizes/Matrizes/Main.java
@@ -0,0 +1,12 @@
+package LogicaDeProgramacaoEmJava.VetoresEMatrizes.Matrizes;
+public class Main {
+  public static void main(String[] args) {
+    int[][] dados = new int[3][3];
+
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        System.out.println(dados[i][j] = j);
+      }
+    }
+  }
+}
