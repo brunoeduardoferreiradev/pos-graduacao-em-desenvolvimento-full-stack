eac8b4da62fdc482cef9afd40916a7d2da76682a
Bruno Eduardo de Moura Ferreira <125761885+brunoeduardoferreiradev@users.noreply.github.com>
2023-03-10 22:20:22 -0300

    Exercicio 01

---
+++ modulo-01/src/LogicaDeProgramacaoEmJava/EstruturasDeRepeticao/Exercicios/Exercicio04/Main.java
@@ -0,0 +1,12 @@
+package LogicaDeProgramacaoEmJava.EstruturasDeRepeticao.Exercicios.Exercicio04;
+
+public class Main {
+  public static void main(String[] args) {
+    int x = 4, y = 8, r = 0;
+
+    for (int i = 0; i < x; i++)
+      r = r + y;
+
+    System.out.println(r);
+  }
+}
