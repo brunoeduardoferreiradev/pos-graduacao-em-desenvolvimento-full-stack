467753050e978909c13b2b3f89c95a7099695002
Bruno Eduardo de Moura Ferreira <125761885+brunoeduardoferreiradev@users.noreply.github.com>
2023-03-10 22:20:56 -0300

    Aula de While

---
+++ modulo-01/src/LogicaDeProgramacaoEmJava/EstruturasDeRepeticao/EstruturaWhile/Main.java
@@ -0,0 +1,16 @@
+package LogicaDeProgramacaoEmJava.EstruturasDeRepeticao.EstruturaWhile;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    int opcao = 0;
+
+    while (opcao != 99) {
+      System.out.println("Digite um valor qualquer ou 99 para sair");
+      Scanner entrada = new Scanner(System.in);
+      opcao = entrada.nextInt();
+    }
+  }
+
+}
