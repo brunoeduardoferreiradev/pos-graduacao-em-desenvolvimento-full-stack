92a34b6656f16221f51c0d006ea3986a14dc8d26
Bruno Eduardo de Moura Ferreira <125761885+brunoeduardoferreiradev@users.noreply.github.com>
2023-03-10 22:35:16 -0300

    Create Main.java

---
+++ modulo-01/src/LogicaDeProgramacaoEmJava/VetoresEMatrizes/Vetores/Main.java
@@ -0,0 +1,12 @@
+package LogicaDeProgramacaoEmJava.VetoresEMatrizes.Vetores;
+public class Main {
+  public static void main(String[] args) {
+    // cria um vetor de inteiros de tamanho 5
+    int[] dados = new int[5];
+    dados[0] = 6;
+    dados[2] = 9;
+    dados[3] = 7;
+    System.out.println(dados[3]); // Mostra o dado gravado na posição 3
+    System.out.println(dados); // Mostra o endereço de memória do vetor
+  }
+}
