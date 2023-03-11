bd34dd5e891c6815311e83985805473bea6a6a40
Bruno Eduardo de Moura Ferreira <125761885+brunoeduardoferreiradev@users.noreply.github.com>
2023-03-10 22:36:23 -0300

    Exercicio 03

---
+++ modulo-01/src/LogicaDeProgramacaoEmJava/VetoresEMatrizes/Exercicios/Exercicio03/Main.java
@@ -0,0 +1,18 @@
+package LogicaDeProgramacaoEmJava.VetoresEMatrizes.Exercicios.Exercicio03;
+public class Main {
+  public static void main(String[] args) {
+     int[] lista = {13,41,5,86,72,9,82,36,27,8};           // Linha 1
+     int i= 0;                                             // Linha 2
+     int pares = 0;                                        // Linha 3 
+     int maiores = 0;                                      // Linha 4
+                                                           // Linha 5
+     //?                                                   // Linha 6
+     //?                                                   // Linha 7
+     pares = pares + 1;                                    // Linha 8
+     if(lista[i] > 28)                                     // Linha 9 
+        maiores = maiores + 1;                             // Linha 10
+                                                           // Linha 11 
+     System.out.println("Pares são: " + pares);            // Linha 12
+     System.out.println("Maiores que 28 são: " + maiores); // Linha 13
+  }
+}
