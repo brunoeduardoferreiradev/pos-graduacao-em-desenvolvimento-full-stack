05a0bc658ba33f17a5fb706d419db196330c1943
Bruno Eduardo de Moura Ferreira <125761885+brunoeduardoferreiradev@users.noreply.github.com>
2023-03-15 21:11:22 -0300

    Aula de HashSet

---
+++ modulo-01/src/LogicaDeProgramacaoEmJava/Collections/HashSet/Main.java
@@ -0,0 +1,18 @@
+package LogicaDeProgramacaoEmJava.Collections.HashSet;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class Main {
+    public static void main(String[] args) {
+        HashSet<Integer> numeros = new HashSet<>();
+        numeros.add(2);
+        numeros.add(5);
+        numeros.add(8);
+
+        Iterator<Integer> it = numeros.iterator();
+        while(it.hasNext()){
+            System.out.println(it.next());
+        }
+    }
+}
