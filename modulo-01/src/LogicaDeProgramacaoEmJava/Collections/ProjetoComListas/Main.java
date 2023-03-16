2b28ff3960588c64f235f73297f2510553bae2fe
Bruno Eduardo de Moura Ferreira <125761885+brunoeduardoferreiradev@users.noreply.github.com>
2023-03-15 21:27:47 -0300

    Aula Projeto com Listas

---
+++ modulo-01/src/LogicaDeProgramacaoEmJava/Collections/ProjetoComListas/Main.java
@@ -0,0 +1,25 @@
+package LogicaDeProgramacaoEmJava.Collections.ProjetoComListas;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> linguagens = new ArrayList<>();
+
+        linguagens.add("Java");
+        linguagens.add("CSharp");
+        linguagens.add("Python");
+        linguagens.add("JavaScript");
+
+        System.out.println("ArrayList: " + linguagens);
+
+        String[] arr = new String[linguagens.size()];
+
+        linguagens.toArray(arr);
+        System.out.println("Array: " );
+
+        for(String item:arr){
+            System.out.println(item + ", ");
+        }
+    }
+}
